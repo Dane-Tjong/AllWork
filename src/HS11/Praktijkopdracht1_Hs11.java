05c2395b285bcc671d73688f18e9869b91729fd1
Dane-Tjong <d.tjong@Live.nl>
2017-12-21 11:31:05 +0100

    .

--- src/HS11/Praktijkopdracht1_Hs11.java
+++ src/HS11/Praktijkopdracht1_Hs11.java
@@ -7,34 +7,44 @@ import java.awt.event.ActionListener;
 
 public class Praktijkopdracht1_Hs11 extends Applet {
     TextField tekstvak;
+    Button okeknop;
     int getal,getal2;
     public void init() {
         tekstvak = new TextField("", 20);
         tekstvak.addActionListener(new TekstvakListener());
         add(tekstvak);
+        okeknop = new Button();
+        okeknop.setLabel("Oke");
+        okeknopListener kn1 = new okeknopListener();
+        okeknop.addActionListener(kn1);
+        add(okeknop);
     }
 
     public void paint(Graphics g) {
         int teller;
         int getal1 = 0;
-        int x = 0;
+        int y = 10;
 
-        for (teller = 0; teller < 12; teller++) {
-            x += 30;
-            g.drawString("" + (getal1*getal2), x, 80);
+        for (teller = 0; teller < 11; teller++) {
+            y += 20;
+            g.drawString(getal1 + " x " + getal2 + " = " + (getal1*getal2), 20, y);
             getal1 = getal1+1;
         }
     }
-
-
+    class okeknopListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            String s = tekstvak.getText();
+            getal = Integer.parseInt( s );
+            getal2 = getal;
+            repaint();
+        }
+    }
     class TekstvakListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             String s = tekstvak.getText();
             getal = Integer.parseInt( s );
             getal2 = getal;
             repaint();
-
-
         }
     }
 
