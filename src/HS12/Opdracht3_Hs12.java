ea85f894c4700fa11818c1f78d551fed854e1558
Dane-Tjong <d.tjong@Live.nl>
2018-01-26 09:57:11 +0100

    .

--- src/HS12/Opdracht3_Hs12.java
+++ src/HS12/Opdracht3_Hs12.java
@@ -1,4 +1,17 @@
 package HS12;
 
-public class Opdracht3_Hs12 {
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht3_Hs12 extends Applet {
+    TextField[] texten = new TextField[5];
+    int teller;
+
+    public void init() {
+        for (TextField textField: texten){
+            teller++;
+            textField = new TextField("ayyy"+teller);
+            add(textField);
+        }
+    }
 }
